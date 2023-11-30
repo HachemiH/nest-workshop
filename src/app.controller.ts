56fa56ef358a36d08e28d4c4dc33bfa15f61c454
Hachemi <hachemi@protonmail.com>
2023-11-30 15:59:08 +0100

    fix: clean route sayGoodbye

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get(':sayGoodbye')
+  @Get('sayGoodbye')
   logicToSayGoodbye(): string {
     return this.appService.sayGoodbye();
   }
